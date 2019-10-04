1616a724d6399c429f52ae22ab30ff967c0e4dee
Tyler Lanphear <animemachine13@gmail.com>
2019-10-03 23:16:35 -0400

    Working IDT with placeholder interrupt handler.

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,7 +1,17 @@
 #include "kernel.h"
 
+#include <stdint.h>
+#include <stddef.h>
+
+#include "idt.h"
+
 void kmain(void)
 {
 loop:
+    load_idt();
+    int j = 1;
+    int i = 0;
+    int k = j / i;
+    (void) k;
     goto loop;
 }
