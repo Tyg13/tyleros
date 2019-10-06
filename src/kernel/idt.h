157e4719bae9d5c16bd4e32587851fffaae11a3c
Tyler Lanphear <animemachine13@gmail.com>
2019-10-05 22:55:59 -0400

    Small format changes.

--- src/kernel/idt.h
+++ src/kernel/idt.h
@@ -6,13 +6,14 @@
 struct IDTR {
     uint16_t limit;
     uint32_t base;
-} __attribute((packed));
+}
+__attribute((packed));
 
 struct IDT_Entry {
     uint16_t offset_1;
     uint16_t selector;
-    uint8_t ist;
-    uint8_t type_attr;
+    uint8_t  ist;
+    uint8_t  type_attr;
     uint16_t offset_2;
     uint32_t offset_3;
     uint32_t zero = 0;
