5d43b328c0b044f7202d64a545aee1b8ca338f91
Tyler Lanphear <animemachine13@gmail.com>
2019-10-07 21:17:16 -0400

    Initial VGA support.

---
+++ src/kernel/vga.cpp
@@ -0,0 +1,66 @@
+#include "vga.h"
+
+#include <stdint.h>
+
+namespace vga {
+
+   const  cursor null_cursor    = { .x = -1, .y = -1 };
+   static cursor current_cursor = { .x =  0, .y =  0 };
+
+   static void write_color_char_at(const char c, color fg, color bg, cursor pos);
+
+   const auto advance_x =
+      [](auto & cursor) {
+         if (++cursor.x >= SCREEN_WIDTH) cursor.x = 0;
+      };
+   const auto advance_y_if_needed =
+      [](auto & cursor) {
+         // If wrapping occurred
+         if (cursor.x == 0) {
+            if (++cursor.y >= SCREEN_HEIGHT) {
+               cursor.y = 0;
+            }
+         }
+      };
+   const auto advance =
+      [](auto & cursor) {
+         advance_x(cursor);
+         advance_y_if_needed(cursor);
+      };
+
+   void string::write() && {
+      if (position != null_cursor) {
+         current_cursor = position;
+      }
+      while (*text != '\0') {
+         char c = *text;
+         if (c == '\n') {
+            current_cursor.x = 0;
+            advance_y_if_needed(current_cursor);
+         } else {
+            write_color_char_at(c, foreground, background, current_cursor);
+            advance(current_cursor);
+         }
+         ++text;
+      }
+   }
+
+   auto vga_memory_base = reinterpret_cast<volatile uint16_t * const>(0xB8000);
+
+   static void write_color_char_at(const char c, color fg, color bg, cursor pos) {
+      auto * screen_pos = vga_memory_base + pos.x + pos.y * SCREEN_WIDTH;
+      auto color = (uint16_t) ((unsigned char) bg << 4 | (unsigned char) fg);
+      *screen_pos = color << 8 | (uint16_t) c;
+   }
+
+   void clear_screen() {
+      auto c = cursor { .x = 0, .y = 0 };
+      for (int i = 0; i < SCREEN_HEIGHT; ++i) {
+         for (int j = 0; j < SCREEN_WIDTH; ++j) {
+            write_color_char_at(' ', color::black, color::black, c);
+            advance(c);
+         }
+      }
+   }
+
+}
---
+++ src/kernel/vga.h
@@ -0,0 +1,60 @@
+#ifndef VGA_H
+#define VGA_H
+
+namespace vga {
+
+   enum class color : unsigned char {
+      black = 0,
+      blue,
+      green,
+      cyan,
+      red,
+      magenta,
+      brown,
+      light_gray,
+      dark_gray,
+      light_blue,
+      light_green,
+      light_cyan,
+      light_red,
+      light_magenta,
+      yellow,
+      white = 0xF,
+   };
+
+   constexpr auto SCREEN_WIDTH  = 80;
+   constexpr auto SCREEN_HEIGHT = 25;
+
+   struct cursor {
+      int x;
+      int y;
+   };
+
+   inline bool operator==(const cursor & lhs, const cursor & rhs) {
+      return lhs.x == rhs.x && lhs.y == rhs.y;
+   }
+   inline bool operator!=(const cursor & lhs, const cursor & rhs) {
+      return !(lhs == rhs);
+   }
+
+   extern const cursor null_cursor;
+
+   class string {
+      const char * text       = nullptr;
+      color        background = color::black;
+      color        foreground = color::white;
+      cursor       position   = null_cursor;
+   public:
+      string() {}
+      string(const char * _text) : text(_text) {}
+      string& fg(color fg_color)      { foreground = fg_color;     return *this; }
+      string& bg(color bg_color)      { background = bg_color;     return *this; }
+      string& at(cursor new_position) { position   = new_position; return *this; }
+      void write() &&;
+   };
+
+   void clear_screen();
+
+}
+
+#endif
