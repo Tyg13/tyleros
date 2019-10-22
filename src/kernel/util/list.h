2ceac45b6c613465b1e96dec21034938afba4dab
Tyler Lanphear <animemachine13@gmail.com>
2019-10-21 21:27:22 -0400

    Fix list implementation.

--- src/kernel/util/list.h
+++ src/kernel/util/list.h
@@ -9,11 +9,23 @@ namespace kstd {
       struct node {
          T    data;
          node * next = nullptr;
-         node * operator++() const { return next; }
+      };
+      struct iterator {
+         iterator(node * _inner) : inner(_inner) {}
+
+         iterator& operator++()       { inner = inner->next; return *this; }
+         T &       operator* () const { return inner->data; }
+
+         friend bool operator!=(const iterator& lhs, const iterator& rhs) { return lhs.inner != rhs.inner; }
+         // Comparison with end iterator is always false
+         friend bool operator!=(T *, const iterator&) { return false; }
+         friend bool operator!=(const iterator&, T *) { return false; }
+      private:
+         node * inner = nullptr;
       };
    public:
-      using iterator       =       node *;
-      using const_iterator = const node *;
+      using iterator       = struct iterator;
+      using const_iterator = const iterator *;
       using allocator_type = Allocator;
       using node_allocator = typename allocator_type::template rebind<node>;
 
@@ -28,26 +40,25 @@ namespace kstd {
       }
 
       void push_back(const T & value) {
-         const auto new_node = [&]() {
-            auto new_node = node_allocator().allocate(sizeof(node));
-            *new_node = node { .data = value, .next = nullptr };
-            return new_node;
-         };
+         node ** where_to_place_new_node;
          if (head == nullptr) {
-            head = new_node();
+            where_to_place_new_node = &head;
          } else {
             while (head->next) {
                head = head->next;
             }
-            head->next = new_node();
+            where_to_place_new_node = &head->next;
          }
+         auto new_node = node_allocator().allocate(sizeof(node));
+         *new_node = node { .data = value, .next = nullptr };
+         *where_to_place_new_node = new_node;
       }
 
-      iterator       begin()       { return head; }
-      const_iterator begin() const { return head; }
+      iterator       begin()       { return iterator      (head); }
+      const_iterator begin() const { return const_iterator(head); }
 
-      iterator       end()       { return nullptr; }
-      const_iterator end() const { return nullptr; }
+      T * end()       { return nullptr; }
+      T * end() const { return nullptr; }
 
       allocator_type get_allocator() const { return allocator_type(); }
 
