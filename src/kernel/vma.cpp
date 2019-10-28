52676ba53ff5bfa582ecd790023aa8d3a47e5d09
Tyler Lanphear <animemachine13@gmail.com>
2019-10-27 22:41:11 -0400

    Prevent kernel and identity pages being lent out by the virtual memory allocator.

--- src/kernel/vma.cpp
+++ src/kernel/vma.cpp
@@ -5,7 +5,7 @@
 #include "pma.h"
 
 struct free_node {
-   void      * address;
+   void      * base;
    size_t      size;
    free_node * next;
 };
@@ -16,16 +16,44 @@ size_t free_list_size;
 size_t max_free_list_size;
 constexpr auto NODES_PER_PAGE = PAGE_SIZE / sizeof(free_node);
 
+static void add_node_to_free_list(void * base, size_t size) {
+   // TODO merge free blocks if they are adjacent
+   free_node * last = virtual_free_list;
+   while (last->next) {
+      last = last->next;
+   }
+
+   auto new_node = last + 1;
+   *new_node = free_node { base, size, nullptr };
+
+   last->next = new_node;
+
+   ++free_list_size;
+}
+
 void init_virtual_memory_allocator() {
+   // Identity map a page for the virtual free list to reside in
    virtual_free_list = reinterpret_cast<decltype(virtual_free_list)>(get_physical_page());
    map_page(virtual_free_list, virtual_free_list);
 
-   virtual_free_list->address = reinterpret_cast<void *>(PAGE_SIZE);
-   virtual_free_list->size    = UINTPTR_MAX - PAGE_SIZE;
-   virtual_free_list->next    = 0;
-
-   free_list_size     = 1;
    max_free_list_size = NODES_PER_PAGE;
+
+   // Initial null entry
+   virtual_free_list->base = 0;
+   virtual_free_list->size = 0;
+   virtual_free_list->next = nullptr;
+   //
+   // TODO: should walk the page tables to determine which virtual addresses
+   // are free, since we identity map pages for the physical frame allocator and
+   // the virtual memory allocator
+
+   // 0x0 - 0x200000 is identity mapped
+   // 0xC000000 - 0xC200000 is the kernel
+   //
+   // Hence 0x200000 - 0xC000000 is free,
+   // and 0xC200000 - UINTPTR_MAX is free
+   add_node_to_free_list(reinterpret_cast<void *>( 0x200000),   0xC000000 -  0x200000);
+   add_node_to_free_list(reinterpret_cast<void *>(0xC200000), UINTPTR_MAX - 0xC200000);
 }
 
 void * get_virtual_pages(size_t n) {
@@ -34,12 +62,12 @@ void * get_virtual_pages(size_t n) {
    for (free_node * node = virtual_free_list; node != nullptr; prev = node, node = node->next) {
       // Found a node big enough
       if (node->size >= n) {
-         const auto free_page_address = node->address;
+         const auto free_page_address = node->base;
          // If the node isn't the exact size of the request, split it
          const auto remaining_size_in_node = node->size - n;
          if (remaining_size_in_node != 0) {
-            node->address = reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(node->address) + n);
-            node->size    = remaining_size_in_node;
+            node->base = reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(node->base) + n);
+            node->size = remaining_size_in_node;
          } else {
             // Remove the node entirely from the list
             if (prev) {
@@ -56,22 +84,6 @@ void * get_virtual_pages(size_t n) {
    }
    return nullptr;
 }
-
-
-static void add_node_to_free_list(void * address, size_t size) {
-   free_node * last = virtual_free_list;
-   while (last->next) {
-      last = last->next;
-   }
-
-   auto new_node = last + 1;
-   *new_node = free_node { address, size, nullptr };
-
-   last->next = new_node;
-
-   ++free_list_size;
-}
-
 
 void free_virtual_pages(void * address, size_t size) {
    add_node_to_free_list(address, size);
