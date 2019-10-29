c9d40e784f28519efa9df0a32cd7329d0c6d4d4b
Tyler Lanphear <animemachine13@gmail.com>
2019-10-28 23:12:06 -0400

    Fix incorrect value for PIC1_DATA.

--- src/kernel/pic.h
+++ src/kernel/pic.h
@@ -6,7 +6,7 @@
 constexpr static uint8_t PIC1_COMMAND = 0x20;
 constexpr static uint8_t PIC1_DATA    = 0x21;
 constexpr static uint8_t PIC2_COMMAND = 0xA0;
-constexpr static uint8_t PIC2_DATA    = 0xA2;
+constexpr static uint8_t PIC2_DATA    = 0xA1;
 
 constexpr static uint8_t END_OF_INTERRUPT = 0x20;
 
