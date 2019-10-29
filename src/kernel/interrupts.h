98521e7f43736d8a510f683234839f0533d70212
Tyler Lanphear <animemachine13@gmail.com>
2019-10-28 23:22:14 -0400

    Make sure interrupt_frame is the correct size.

--- src/kernel/interrupts.h
+++ src/kernel/interrupts.h
@@ -8,12 +8,13 @@ typedef size_t uword_t __attribute__((mode (__word__)));
 
 struct interrupt_frame
 {
-    uword_t ip;
+    uword_t rip;
     uword_t cs;
-    uword_t flags;
-    uword_t sp;
+    uword_t rflags;
+    uword_t rsp;
     uword_t ss;
-};
+} __attribute__((packed));
+static_assert(sizeof(interrupt_frame) == 40);
 
 uintptr_t get_interrupt_handler(unsigned int vector_index);
 
