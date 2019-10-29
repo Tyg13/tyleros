14f5010d016991a24a5eec59da180907d6a7d57f
Tyler Lanphear <animemachine13@gmail.com>
2019-10-28 23:13:40 -0400

    Set KERNEL_VMA_OFFSET to 0 for interrupt debugging purposes.

--- src/kernel/idt.cpp
+++ src/kernel/idt.cpp
@@ -18,24 +18,28 @@ void init_interrupts() {
    remap_pic();
    load_idt();
    init_pit();
+   asm volatile ("sti" ::: "memory", "cc");
 }
 
 void load_idt() {
-    for (unsigned int i = 0; i < IDT_Size; ++i) {
-       auto handler = reinterpret_cast<uintptr_t>(get_interrupt_handler(i));
-       idt[i] = {
-          .offset_1  = (uint16_t)(handler),
-          .selector  = 0x8,
-          .ist       = 0,
-          .type_attr = (uint8_t) IDT_Entry::Attr::present | (uint8_t) IDT_Entry::Type::interrupt_32,
-          .offset_2  = (uint16_t)(handler >> 16),
-          .offset_3  = (uint32_t)(handler >> 32),
-       };
-    }
-    g_idtr = {
-       .limit = sizeof(idt) - 1,
-       .base = reinterpret_cast<uintptr_t>(&idt),
-    };
-    asm volatile ("lidt %0\n\t"
-                  "sti" :: "m"(g_idtr));
+   const auto KERNEL_VMA_OFFSET = 0;
+   for (unsigned int i = 0; i < IDT_Size; ++i) {
+      auto handler = reinterpret_cast<uintptr_t>(get_interrupt_handler(i)) - KERNEL_VMA_OFFSET;
+      idt[i] = {
+         .offset_1  = (uint16_t)(handler),
+         .selector  = 0x8,
+         .ist       = 0,
+         .type_attr = (uint8_t) IDT_Entry::Attr::present | (uint8_t) IDT_Entry::Type::interrupt_32,
+         .offset_2  = (uint16_t)(handler >> 16),
+         .offset_3  = (uint32_t)(handler >> 32),
+      };
+   }
+   g_idtr = {
+      .limit = sizeof(idt) - 1,
+      .base = reinterpret_cast<uintptr_t>(&idt) - KERNEL_VMA_OFFSET,
+   };
+
+   IDTR * g_idtr_address =
+      reinterpret_cast<IDTR *>(reinterpret_cast<uintptr_t>(&g_idtr) - KERNEL_VMA_OFFSET);
+   asm volatile ("lidt %0\n\t" :: "m"(*g_idtr_address));
 }
