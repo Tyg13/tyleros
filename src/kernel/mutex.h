27bee6bb19a032f30f16dfb9757acb7303896516
Tyler Lanphear <animemachine13@gmail.com>
2019-11-01 21:33:00 -0400

    Add initial mutex support.

---
+++ src/kernel/mutex.h
@@ -0,0 +1,27 @@
+#ifndef MUTEX_H
+#define MUTEX_H
+
+#include "scheduler.h"
+
+struct mutex {
+   mutex() {}
+   void acquire() {
+      while (!__sync_bool_compare_and_swap(&locked, false, true)) {
+         yield();
+      }
+   }
+   void release() {
+      locked = false;
+   }
+private:
+   bool locked = false;
+};
+
+struct mutex_guard {
+    mutex_guard(mutex& mtx) : mtx(mtx) { mtx.acquire(); }
+   ~mutex_guard() { mtx.release(); }
+private:
+   mutex& mtx;
+};
+
+#endif
