5158584ccb21ca66b99b09b9f578df29ddea13f4
Tyler Lanphear <animemachine13@gmail.com>
2019-11-18 22:33:49 -0500

    libc: add strncpy, strcmp, strchr.

--- src/libc/string.cpp
+++ src/libc/string.cpp
@@ -1,5 +1,7 @@
 #include <string.h>
 
+#include <stdint.h>
+
 template <typename T> struct volatile_helper_impl               { using type = unsigned char; };
 template <typename T> struct volatile_helper_impl<volatile T *> { using type = volatile unsigned char; };
 template <typename T> using volatile_helper = typename volatile_helper_impl<T>::type;
@@ -27,11 +29,15 @@ static inline auto memset_impl(T * dst, int c, size_t n) {
    return __dst;
 };
 
-void * memcpy(void * dst, const void * src, size_t n) {
+volatile void * memcpy_v(volatile void * dst, const volatile void * src, size_t n) {
    return memcpy_impl(dst, src, n);
 }
 
-volatile void * memcpy_v(volatile void * dst, const volatile void * src, size_t n) {
+volatile void * memset_v(volatile void * dst, int c, size_t n) {
+   return memset_impl(dst, c, n);
+}
+
+void * memcpy(void * dst, const void * src, size_t n) {
    return memcpy_impl(dst, src, n);
 }
 
@@ -39,8 +45,38 @@ void * memset(void * dst, int c, size_t n) {
    return memset_impl(dst, c, n);
 }
 
-volatile void * memset_v(volatile void * dst, int c, size_t n) {
-   return memset_impl(dst, c, n);
+char * strchr(const char * str, int character) {
+   if (str) {
+      for (int i = 0; str[i] != '\0'; ++i) {
+         if (str[i] == (char) character) {
+            return const_cast<char *>(&str[i]);
+         }
+      }
+   }
+   return nullptr;
+}
+
+int strcmp(const char * str1, const char * str2) {
+   for (int i = 0; str1[i] != '\0' && str2[i] != '\0'; ++i) {
+      if (str1[i] != str2[i]) {
+         return str1[i] > str2[i] ? -1 : 1;
+      }
+   }
+   return 0;
+}
+
+char * strncpy(char * dst, const char * src, size_t n) {
+   for (size_t i = 0; i < n; ++i) {
+      *dst = *src;
+      if (src[i] == '\0') {
+         break;
+      }
+   }
+   return dst;
+}
+
+char * strcpy(char * dst, const char * src) {
+   return strncpy(dst, src, SIZE_MAX);
 }
 
 size_t strlen(const char * src) {
@@ -49,16 +85,6 @@ size_t strlen(const char * src) {
       ++len;
    }
    return len;
-}
-
-char * strcpy(char * dst, const char * src) {
-   for (int i = 0; ; ++i) {
-      *dst = *src;
-      if (src[i] == '\0') {
-         break;
-      }
-   }
-   return dst;
 }
 
 char * strrev(char * src, int len) {
--- src/libc/string.h
+++ src/libc/string.h
@@ -7,13 +7,16 @@
 extern "C" {
 #endif
 
-void * memcpy(void * src, const void * dst, size_t n);
 volatile void * memcpy_v(volatile void * src, const volatile void * dst, size_t n);
-void * memset(void * dst, int c, size_t n);
 volatile void * memset_v(volatile void * dst, int c, size_t n);
-size_t strlen(const char *);
-char * strcpy(char * dst, const char * src);
+void * memcpy(void * src, const void * dst, size_t n);
+void * memset(void * dst, int c, size_t n);
 
+char * strchr(const char * str, int character);
+int    strcmp(const char * str1, const char * str2);
+char * strncpy(char * dst, const char * src, size_t n);
+char * strcpy(char * dst, const char * src);
+size_t strlen(const char *);
 char * strrev(char * src, int len);
 
 #ifdef __cplusplus
