ed4460ce6e1e74b610552f9cf0aeabe3413ab30f
Tyler Lanphear <animemachine13@gmail.com>
2020-01-10 21:49:37 -0500

    kernel: add preliminary path support to filesystem

--- src/kernel/filesystem.cpp
+++ src/kernel/filesystem.cpp
@@ -1,71 +1,13 @@
 #include "filesystem.h"
 
+#include "util.h"
 #include "floppy.h"
 
-#include <stdint.h>
+#include <string.h>
 
-struct bios_parameter_block {
-   uint8_t  jumpcode  [3];
-   char     identifier[8];
-   uint16_t bytes_per_sector;
-   uint8_t  sectors_per_cluster;
-   uint16_t num_of_reserved_sectors;
-   uint8_t  num_of_fats;
-   uint16_t num_of_directory_entries;
-   uint16_t small_sector_count;
-   uint8_t  media_descriptor_type;
-   uint16_t sectors_per_fat;
-   uint16_t sectors_per_track;
-   uint16_t num_of_heads;
-   uint32_t num_of_hidden_sectors;
-   uint32_t large_sector_count;
-   struct {
-      uint8_t  drive_number;
-      uint8_t  reserved;
-      uint8_t  signature;
-      uint32_t serial_number;
-      char     volume_label     [11];
-      char     system_identifier[8];
-   } __attribute__((packed));
-} __attribute__((packed));
-static_assert(sizeof(bios_parameter_block) == 62);
-
-struct directory_entry {
-   char short_file_name[11];
-   enum : uint8_t {
-      READ_ONLY      = 0x01,
-      HIDDEN         = 0x02,
-      SYSTEM         = 0x04,
-      VOLUME_ID      = 0x08,
-      DIRECTORY      = 0x10,
-      ARCHIVE        = 0x20,
-      LONG_FILE_NAME = READ_ONLY | HIDDEN | SYSTEM | VOLUME_ID,
-   } attributes;
-   uint8_t reserved;
-   uint8_t creation_time_deciseconds;
-   uint16_t creation_hour        : 5;
-   uint16_t creation_minutes     : 6;
-   uint16_t creation_halfseconds : 5;
-   uint16_t creation_year        : 7;
-   uint16_t creation_month       : 4;
-   uint16_t creation_day         : 5;
-   uint16_t accessed_year        : 7;
-   uint16_t accessed_month       : 4;
-   uint16_t accessed_day         : 5;
-   uint16_t cluster_num_hi;
-   uint16_t modified_hour        : 5;
-   uint16_t modified_minutes     : 6;
-   uint16_t modified_halfseconds : 5;
-   uint16_t modified_year        : 7;
-   uint16_t modified_month       : 4;
-   uint16_t modified_day         : 5;
-   uint16_t cluster_num_lo;
-   uint32_t size;
-} __attribute__((packed));
-static_assert(sizeof(directory_entry) == 32);
-
-bios_parameter_block bpb;
-directory_entry * root_dir;
+bios_parameter_block * bpb      = nullptr;
+directory_entry      * root_dir = nullptr;
+char                 * fat      = nullptr;
 
 static void read_boot_sector();
 static void read_root_directory();
@@ -76,17 +18,93 @@ void init_filesystem() {
 }
 
 void read_boot_sector() {
-   read_floppy_sector(0);
-   bpb = *reinterpret_cast<const bios_parameter_block *>(floppy_dma_buffer);
+   bpb = new bios_parameter_block;
+   read_floppy(bpb, 0, 1);
 }
 
 void read_root_directory() {
-   const auto root_directory_sector = bpb.num_of_reserved_sectors
-                                    + bpb.sectors_per_fat * bpb.num_of_fats;
-   root_dir = new directory_entry[bpb.num_of_directory_entries];
-   read_floppy_sector(root_directory_sector);
-   for (int i = 0; i < bpb.num_of_directory_entries; ++i) {
-      const auto entry_ptr = reinterpret_cast<const directory_entry *>(floppy_dma_buffer) + i;
-      root_dir[i] = *entry_ptr;
+   const auto fat_sectors                 = bpb->sectors_per_fat * bpb->num_of_fats;
+   const auto root_directory_sector       = bpb->num_of_reserved_sectors + fat_sectors;
+   const auto root_directory_size         = sizeof(directory_entry) * bpb->num_of_directory_entries;
+   const auto root_directory_sector_count = root_directory_size / bpb->bytes_per_sector;
+
+   root_dir = new directory_entry[bpb->num_of_directory_entries];
+   read_floppy(root_dir, root_directory_sector, root_directory_sector_count);
+}
+
+static const directory_entry * resolve_path(const char * path) {
+   assert(path && path[0] == '/', "Tried to read file: %s", path ? path : "null");
+   const char * rest = &path[1];
+
+   char fragment[sizeof(directory_entry::short_file_full_name) + 1];
+   auto read_next_fragment = [&](auto & rest) {
+      const char * dir_sep = strchr(rest, '/');
+      const auto rest_size = dir_sep ? dir_sep - rest : strlen(rest);
+      size_t fragment_size = min(sizeof(fragment) - 1, rest_size);
+
+      strncpy(fragment, rest, fragment_size);
+      fragment[fragment_size + 1] = '\0';
+
+      rest = rest + fragment_size;
+      return fragment;
+   };
+
+   read_next_fragment(rest);
+
+   const directory_entry * file_entry = nullptr;
+   const directory_entry * directory = root_dir;
+   const auto directory_size = bpb->num_of_directory_entries;
+   for (auto i = 0; i < directory_size; ++i) {
+      const auto & entry = directory[i];
+      if (strcmp(entry.short_file_full_name, fragment) == 0) {
+         // Path fully resolved
+         if (rest[0] == '\0') {
+            file_entry = &entry;
+            break;
+         }
+         assert(entry.attributes & directory_entry::DIRECTORY,
+                "Path fragment not directory: %s (%s)", fragment, rest);
+
+         read_next_fragment(rest);
+         i = 0;
+         directory = &entry;
+      }
    }
+   assert(file_entry != nullptr, "Couldn't resolve path %s", path);
+   return file_entry;
+}
+
+void * read_file(const char * path) {
+   const directory_entry * file_entry = resolve_path(path);
+   auto buffer = new char[file_entry->size];
+   if (!fat) {
+      fat = new char[bpb->sectors_per_fat * bpb->bytes_per_sector];
+   }
+   const auto first_fat_sector        = bpb->num_of_reserved_sectors;
+   const auto num_of_fat_sectors      = bpb->sectors_per_fat * bpb->num_of_fats;
+   const auto size_of_root_dir        = bpb->num_of_directory_entries * sizeof(directory_entry);
+   const auto num_of_root_dir_sectors = div_round_up(size_of_root_dir, bpb->bytes_per_sector);
+   const auto first_data_sector       = first_fat_sector + num_of_fat_sectors + num_of_root_dir_sectors;
+
+   auto     offset    = 0;
+   auto     cluster   = file_entry->cluster_num_lo;
+   uint16_t fat_value = 0;
+   while (cluster < 0xFF8) {
+      const auto fat_offset   = cluster + (cluster / 2);
+      const auto fat_sector   = first_fat_sector + (fat_offset / bpb->bytes_per_sector);
+      const auto entry_offset = fat_offset % bpb->bytes_per_sector;
+      read_floppy(fat, fat_sector, 1);
+
+      const auto data_sector = ((cluster - 2) * bpb->sectors_per_cluster) + first_data_sector;
+      read_floppy(buffer + offset, data_sector, 1);
+
+      offset += bpb->bytes_per_sector;
+
+      const auto * fat_entry = reinterpret_cast<uint16_t *>(&fat[entry_offset]);
+      fat_value = *fat_entry;
+      cluster = (cluster & 1) ? fat_value >> 4
+                              : fat_value & 0x0FFF;
+   }
+
+   return buffer;
 }
