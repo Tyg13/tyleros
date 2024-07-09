d2e76c91932b601706b75a07425d4c3c52fed179
Tyler Lanphear <tylanphear@gmail.com>
2024-07-09 02:50:11 -0700

    libc: declarations for feof/ferror/SEEK_SET, rearrange f* functions

--- src/libc/stdio.h
+++ src/libc/stdio.h
@@ -9,6 +9,7 @@ extern "C" {
 #include <stddef.h>
 
 #define SEEK_SET 0
+#define SEEK_END -1
 
 typedef struct FILE {
   int desc;
@@ -21,16 +22,20 @@ extern FILE *stderr;
 int fclose(FILE *);
 int fflush(FILE *);
 FILE *fopen(const char *, const char *);
+size_t fread(void *, size_t, size_t, FILE *);
+int fseek(FILE *, long, int);
+long ftell(FILE *);
+size_t fwrite(const void *, size_t, size_t, FILE *);
+int feof(FILE *);
+int ferror(FILE *);
+
 int printf(const char *, ...)
     __attribute__((format(printf, 1, 2)));
 int vprintf(const char *, va_list args);
 int fprintf(FILE *, const char *, ...)
     __attribute__((format(printf, 2, 3)));
 int vfprintf(FILE *, const char *, va_list);
-size_t fread(void *, size_t, size_t, FILE *);
-int fseek(FILE *, long, int);
-long ftell(FILE *);
-size_t fwrite(const void *, size_t, size_t, FILE *);
+
 void setbuf(FILE *, char *);
 
 int vsprintf(char *str, const char *fmt, va_list args);
